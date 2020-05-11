daa16c287d5fa02558d65c02ddfa58d209b34305
Andrew Senapatiratne <senapaat1456@uwec.edu>
2020-05-11 17:13:21 -0500

    fixed database tables

--- note-tex-database/tableSetup.sql
+++ note-tex-database/tableSetup.sql
@@ -3,14 +3,16 @@ DROP TABLE IF EXISTS users;
 
 CREATE TABLE noteSheets(
     noteSheet_id SERIAL PRIMARY KEY,
+    useCreator TEXT,
     lineCount INT,
     contents TEXT,
-    is_deleted INT DEFAULT 0,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    isDeleted INT DEFAULT 0,
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (userCreator) REFERENCES users(userName)
 );
 
 CREATE TABLE users(
-    user_id SERIAL PRIMARY KEY,
-    user_name TEXTS
+    userId SERIAL PRIMARY KEY,
+    userName TEXT
 );
