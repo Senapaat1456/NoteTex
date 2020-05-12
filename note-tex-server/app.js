9d53c545b8346b9d01080e30a3fa642080365677
Andrew Senapatiratne <senapaat1456@uwec.edu>
2020-05-12 11:55:07 -0500

    added patch and delete

--- note-tex-server/app.js
+++ note-tex-server/app.js
@@ -17,12 +17,16 @@ function rowToNoteSheetName(row){
   }
 }
 
-function rowToUserId(row){
+function rowToNoteSheet(row){
   return{
-    userId:row.userId
+    notSheet_id:row.noteSheet_id,
+    lineCount:row.lineCount,
+    contents:row.contents,
+    updatedAt:row.updatedAt
   }
 }
 
+//gets list of note sheets owned by a user
 app.get('/noteSheetList/:userName',(request, responce) => {
   const query = 'SELECT noteSheetName FROM noteSheets JOIN users ON userCreator = userId WHERE isDeleted = 0 AND userName = ?';
   const params = [request.params.userName];
@@ -34,6 +38,7 @@ app.get('/noteSheetList/:userName',(request, responce) => {
   });
 });
 
+//get a noteSheet that belong to a user
 //note, the sheet must be owned by the user
 app.get('/noteSheet/:userName/:sheetName',(request, responce) => {
   const query = 'SELECT noteSheetName FROM noteSheets JOIN users ON userCreator = userId WHERE isDeleted = 0 AND userName = ? AND noteSheetName = sheetName';
@@ -41,24 +46,18 @@ app.get('/noteSheet/:userName/:sheetName',(request, responce) => {
   connection.query(query, params, (error, rows) => {
     responce.send({
       ok: true,
-      usersNoteSheets: rows
+      usersNoteSheets: rows.map(rowToNoteSheet)
     });
   });
 });
 
+//new note sheet
 app.post('/noteSheetList',(request, responce) => {
-  console.log("received request: " + JSON.stringify(request.body));
   const userIdQuery = 'SELECT userId FROM users WHERE userName = ?';
   const userIdParms = [request.body.userName];
-  console.log("userName: " + request.body.userName);
   var userId = -1;
   userId = connection.query(userIdQuery,userIdParms, (errors,rows) =>{
-    console.log(rows[0]);
-    console.log(typeof(rows[0].userId));
     userId = rows[0].userId;
-    console.log("type of userId: "  + typeof(userId));
-  
-    console.log("fain: " + userId)
     const query = 'INSERT INTO noteSheets (noteSheetName, userCreator, lineCount, contents) values (?,?,?,?)';
     const params = [request.body.sheetName,userId,request.body.lineCount,request.body.contents];
       connection.query(query, params, (error, result) => {
@@ -69,6 +68,54 @@ app.post('/noteSheetList',(request, responce) => {
       });
     });
   });
+});
+
+app.post('/users'), (request, repsonce) => {
+  const newUserInsert = "INSERT INTO users (userName) VALUES (?)";
+  const newUserParams = [request.body.userName];
+  connection.query(newUserInsert, newUserParams, (error, result) => {
+    responce.send({
+      ok:true,
+      id: result.insertId
+    })
+  });
+});
+
+app.patch('/noteSheet', (request, responce) => {
+  const userIdQuery = 'SELECT userId FROM users WHERE userName = ?';
+  const userIdParms = [request.body.userName];
+  var userId = -1;
+  userId = connection.query(userIdQuery,userIdParms, (errors,rows) =>{
+    userId = rows[0].userId;
+    const noteSheetUpdate = 'UPDATE noteSheets SET contents = ?, lineCount = ? updatedAt = CURRENT_TIMESTAMP WHERE notSheet_id = ? AND userCreator = ?'
+    const params = [request.body.contents,userId,request.body.lineCount,request.body.contents,request.body.notSheet_id, userId];
+      connection.query(query, params, (error, result) => {
+        console.log(error);
+      responce.send({
+          ok: true,
+      });
+    });
+  });
+  });
+
+  app.delete('/noteSheet', (request, responce) =>{
+    const userIdQuery = 'SELECT userId FROM users WHERE userName = ?';
+    const userIdParms = [request.body.userName];
+    var userId = -1;
+    userId = connection.query(userIdQuery,userIdParms, (errors,rows) =>{
+      userId = rows[0].userId;
+      const noteSheetUpdate = 'UPDATE noteSheets SET isDeleted = 1 updatedAt = CURRENT_TIMESTAMP WHERE notSheet_id = ? AND userCreator = ?'
+      const params = [,request.body.notSheet_id, userId];
+        connection.query(query, params, (error, result) => {
+          console.log(error);
+        responce.send({
+            ok: true,
+        });
+      });
+    });
+  });
+
+  const noteSheetUpdate = 'UPDATE noteSheets SET contents = ?, updatedAt = CURRENT_TIMESTAMP WHERE '
 });
 
 const port = 1443;
