bb8bdaee20b17862ce6799da36d994442e3fac10
Andrew Senapatiratne <senapaat1456@uwec.edu>
2020-05-12 00:41:22 -0500

    Updated app

--- note-tex-server/app.js
+++ note-tex-server/app.js
@@ -7,6 +7,27 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+let credentials = JSON.parse(fs.readFileSync('credentials.json','utf8'));
+let connection = mysql.createConnection(credentials);
+connection.connect();
+
+function rowToNotSheetName(row){
+  return{
+    noteSheetName:row.noteSheetName;
+  }
+}
+
+app.get('/noteSheetList/:userName',(request, responce) => {
+  const query = 'SELECT noteSheetName N FROM noteSheets JOIN users U ON N.userCreator = U.userId WHERE isDeleted = 0 AND U.userName = ?'
+  const params = [request.params.userName];
+  connection.query(query, params, (error, rows) => {
+    responce.send({
+      ok: true,
+      memories: row.map(rowToObject)
+    });
+  });
+});
+
 const port = 443;
 app.listen(port, () => {
   console.log("Running on port: " + port);
