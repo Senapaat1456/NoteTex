d9993a676eec3c77c917b23cf439a527ecb0b947
Andrew Senapatiratne <senapaat1456@uwec.edu>
2020-05-12 10:28:03 -0500

    added post request for new sheet

--- note-tex-server/app.js
+++ note-tex-server/app.js
@@ -28,6 +28,35 @@ app.get('/noteSheetList/:userName',(request, responce) => {
   });
 });
 
+//note, the sheet must be owned by the user
+app.get('/noteSheet/:userName/:sheetName',(request, responce) => {
+  const query = 'SELECT noteSheetName FROM noteSheets JOIN users ON userCreator = userId WHERE isDeleted = 0 AND userName = ? AND noteSheetName = sheetName';
+  const params = [request.params.userName];
+  connection.query(query, params, (error, rows) => {
+    responce.send({
+      ok: true,
+      usersNoteSheets: rows
+    });
+  });
+});
+
+app.pos('/noteSheetList',(request, responce) => {
+  const userIdQuery = 'SELECT userId FROM users WHERE userName = ?';
+  const userIdParms = [request.body.userName];
+  const id;
+  connection.query(userIdQuery,userIdParms, (errors,rows) =>{
+    userId = rows.userId
+  })
+  const query = 'INSERT INTO noteSheets (noteSheetName, userCreator, lineCount, contents) values (?,?,?,?)';
+  const params = [request.body.sheetName,userId,request.body.lineCount,request.body.contents];
+  connection.query(query, params, (error, result) => {
+    responce.send({
+      ok: true,
+      id: result.insertId
+    });
+  });
+});
+
 const port = 1443;
 app.listen(port, () => {
   console.log("Running on port: " + port);
