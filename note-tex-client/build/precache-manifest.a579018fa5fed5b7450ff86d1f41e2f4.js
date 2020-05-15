e9aede6122f8a34813f74506097dd4e2f8dc7964
Andrew Senapatiratne <senapaat1456@uwec.edu>
2020-05-14 21:11:06 -0500

    built

---
+++ note-tex-client/build/precache-manifest.a579018fa5fed5b7450ff86d1f41e2f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e34c969dcc9852980f7976d09e71d78b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b14b8fc9701fab34d36e",
+    "url": "/static/css/main.f69624aa.chunk.css"
+  },
+  {
+    "revision": "d03a3d5ed71b03dfabac",
+    "url": "/static/js/2.aeb4dbfa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.aeb4dbfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b14b8fc9701fab34d36e",
+    "url": "/static/js/main.eaaba39c.chunk.js"
+  },
+  {
+    "revision": "d828c05e378b2c591bb7",
+    "url": "/static/js/runtime-main.aef00d6a.js"
+  }
+]);
